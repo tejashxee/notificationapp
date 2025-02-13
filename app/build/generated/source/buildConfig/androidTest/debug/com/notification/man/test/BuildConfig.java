6827848227f5822cf60de71732ef511ca1b0b1e3
tejashxee <tejashreemestry0601@gmail.com>
2025-02-13 22:21:37 +0530

    Fixed .gitignore and removed unnecessary files

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/notification/man/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.notification.man.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.notification.man.test";
+  public static final String BUILD_TYPE = "debug";
+}
